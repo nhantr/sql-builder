b8ee5e15ab32128abe8e8b9dd4e3f7e2bfc6c1cd
nhantr <33366385+nhantr@users.noreply.github.com>
2022-09-22 13:26:05 +0700

    add TestNG, setup test cases

---
+++ src/test/java/ml/egoztyle/builder/sql/InsertQueryBuilderTest.java
@@ -0,0 +1,18 @@
+package ml.egoztyle.builder.sql;
+
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ *
+ * @author ttnhan
+ * @version $Revision:  $
+ */
+public class InsertQueryBuilderTest
+{
+    @Test
+    public void f()
+    {
+    }
+}
